8c181b3ff8b6e05efe2e9d13d82c9851fdd8adcd
Kevin Hutson <Kevin.Hutson@blackbaud.com>
2018-10-07 18:50:20 -0500

    Refactor, remove rjxs and adopt callback style for parsing. Simplifies and enabled us to add cleanup for when parsing is complete

--- save_lag.js
+++ save_lag.js
@@ -3,30 +3,46 @@ const sql = require('./lib/sql');
 var Queue = require('better-queue');
 let PropertiesReader = require('properties-reader');
 var _ = require('lodash');
+var fs = require('fs');
+const path = require('path');
 
 let properties = PropertiesReader('settings.properties');
 let lagDir = properties.get('main.lag.files.dir');
 
+function delete_lag_files(directory, callback) {
+  console.log('>>> delete_lag_files directory='+directory);
+  fs.readdirSync(directory).forEach(file => {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    });
+}
+
 // Perform a database save for each task's "completion"
-function task_done(err,result) {
+function save_lag_item(err,result) {
+  //console.log('>> saving result='+JSON.stringify(result));
   sql.save_lag(result);
+}
+
+// Clean up our lag directory after completion so we don't re-process the same data
+function save_complete(err,result) {
+  console.log('>> save_complete='+JSON.stringify(result));
+  delete_lag_files(lagDir);
 }
 
 // parse and feed it some data, using Queue to slow down the processing 
 // using `afterProcessDelay` so that our sqlite3 database can handle it.
 function process_lag_results(lagDir) {
   var q = new Queue((input, cb) => {
-    //console.log('processing...input='+input);
     cb(null, input);
-  }, { afterProcessDelay: 200 });
+  }, { afterProcessDelay: 100 });
   
-  let obs = lagParser.parse_lag(lagDir);
-  _.forEach(obs, function(lag_observable) {
-    lag_observable.subscribe(x => {
-        q.push(x, task_done);
-    });
+  lagParser.parse(lagDir, (lag_items) => {
+    _.forEach(lag_items, function(lag_item) {
+      q.push(lag_item, save_lag_item);
+    });  
+    q.push('Job complete.', save_complete);
   });
 }
 
 process_lag_results(lagDir);
-console.log('done!');
