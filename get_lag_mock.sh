391017cc8e3da9e537bc52e7101b0a1f0c9c29c1
Kevin Hutson <kevin.hutson@blackbaud.com>
2018-10-28 12:02:16 -0500

    Implement method to remove expired dataStore files according to datastore.retention.time.ms setting

--- get_lag_mock.sh
+++ get_lag_mock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # simulate output from our script by just cat'ing out the files from a test directory
-for filename in cron-out3/*.txt; do
+for filename in samples/*.txt; do
     cat $filename
 done
